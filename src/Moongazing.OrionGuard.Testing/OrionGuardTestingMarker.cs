4de109c473ecc7d61186005809818214bb794610
Moongazing <tunahan.ali.ozturk@outlook.com>
2026-05-13 11:23:15 +0300

    polish(testing): delete obsolete OrionGuardTestingMarker placeholder

--- src/Moongazing.OrionGuard.Testing/OrionGuardTestingMarker.cs
+++
@@ -1,4 +0,0 @@
-namespace Moongazing.OrionGuard.Testing;
-
-/// <summary>Type marker used by assembly scanning. Replaced by real types in subsequent tasks.</summary>
-internal static class OrionGuardTestingMarker { }
